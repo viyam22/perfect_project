9e8491f9b120816285a71ba8d6136b7f554a6340
viyam <1020793720@qq.com>
2017-08-12 14:26:18 +0800

    去掉阿拉丁数据统计

--- utils/ald-stat-conf.js
+++
@@ -1,5 +0,0 @@
-exports.app_key = "30771c73fe75da71ab2a128fb0354dc2"; //请在此行填写从阿拉丁后台获取的appkey
-exports.getLocation = false; //默认不获取用户坐标位置
-exports.getUserinfo = false; //默认不获取用户头像昵称
-exports.appid = ""; //用于用户登录、微信转发群信息、二维码等微信官方功能
-exports.appsecret = "";//用于用户登录、微信转发群信息、二维码等微信官方功能
